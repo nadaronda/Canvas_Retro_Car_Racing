f4922ac0053cbb7a2cb78a01135749328edc195e
Saray Rosario Plaza <you@example.com>
2023-02-03 19:20:44 +0100

    add html y css

--- src/script.js
+++ src/script.js
@@ -2,4 +2,10 @@ window.onload = () => {
     
     var canvas = document.getElementById("canvas") 
     var ctx = canvas.getContext('2d');
+
+    ctx.beginPath();
+    ctx.rect(10, 10, 10, 10);
+    ctx.fillStyle = "#FF0000";
+    ctx.fill();
+    ctx.closePath();
 }
