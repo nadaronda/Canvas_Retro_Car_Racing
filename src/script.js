45d78bb358caeceb39ac851e464e52b72f3b9fa7
Saray Rosario Plaza <you@example.com>
2023-02-06 17:34:48 +0100

    ver cosas

--- src/script.js
+++ src/script.js
@@ -4,8 +4,10 @@ window.onload = () => {
     var ctx = canvas.getContext('2d');
 
     ctx.beginPath();
+
     ctx.rect(10, 10, 10, 10);
     ctx.fillStyle = "#FF0000";
     ctx.fill();
+    
     ctx.closePath();
 }
